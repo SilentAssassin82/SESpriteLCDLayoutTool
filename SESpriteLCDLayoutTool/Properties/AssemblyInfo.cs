3b1c3925262b0a753e915c6dd07774b98b642a3d
SilentAssassin82 <chriskelly1982@msn.com>
2026-04-02 06:03:36 +0100

    version 1.1

--- SESpriteLCDLayoutTool/Properties/AssemblyInfo.cs
+++ SESpriteLCDLayoutTool/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SESpriteLCDLayoutTool")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("WYSIWYG visual editor for Space Engineers LCD sprite layouts")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SESpriteLCDLayoutTool")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
